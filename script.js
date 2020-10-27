4185dd0f1de30d0200a92c3eecdef8578334b97f
Gustavo <gustavocariocasilva49@outlook.com>
2020-10-26 21:12:19 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//selecionando os movimentos para fazer acao
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+//seleção dos items para o 3d
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+//evento de movimentação
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+
+//entrada da animação
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  
+  //segurando os hs para pular na tela
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+
+
+//saída da animação quando soltar o mouse 
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  
+  //descrições e outros voltam para o normal 
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
